6a7ac7cb4da64b72d78d99dddd7d92161ce20578
Özkan Demir <ozkanndemir2009@gmail.com>
2024-03-29 23:27:58 +0300

    login and register finished

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -1,11 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+  const { userId } = auth();
   return (
     <div>
       <UserButton />
-      Anasayfa
+      {userId ? "Tebrikler girişiniz başarılı" : "Giriş başarısız"}
     </div>
   );
 }
